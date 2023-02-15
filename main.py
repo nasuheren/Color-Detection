c0bed512a445891aada3e3ca75594912f13c9687
nasuheren <nasuherendemirci@gmail.com>
2023-02-15 23:23:32 +0300

    Color Deteciton (Red)

---
+++ main.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+def averageColor(img):
+    avgColorPerRow = np.average(img, axis=0)
+    avgColor = np.average(avgColorPerRow, axis=0)
+    # opencv'de renkler RGB yerine BGR olarak sıralandığı için 2,1,0 diye sıralandı yani 2=B,1=G,0=R.
+    myColor = (int(avgColor[2]), int(avgColor[1]), int(avgColor[0]))
+    return myColor
+
+while True:
+
+    # kameradan alınan görüntüleri "frame"e kayıt eder.
+    ret, frame = cap.read()
+
+    # ayna etkisi yapmak için.
+    frame = cv2.flip(frame, 1)
+
+    frame = cv2.medianBlur(frame, 5)
+
+    # BGR dan HSV formatına çeviriyoruz.
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # hsv formatında kırmızı rengin alt ve üst sınırları.
+    lower_red = np.array([161,155,84])
+    upper_red = np.array([179,255,255])
+
+    # rengi maskeleme işlemi.
+    red_mask = cv2.inRange(hsv_frame, lower_red, upper_red)
+
+    # kırmızı renge maskeleme işlemi.
+    red_red_mask = cv2.bitwise_and(frame, frame, mask = red_mask)
+
+    dilate = cv2.dilate(red_mask, (3,3), iterations=3)
+
+    contour, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for i in range(len(contour)):
+        cv2.drawContours(frame, contour, i, (0,255,255), 4)
+
+    # kameradan alınan görüntüleri ekrana basmaya yarıyor.
+    cv2.imshow("Camera", frame)
+    # maskelenmiş kamera görüntüsü.
+    cv2.imshow("Mask Camera", red_mask)
+    # kırmızı maskelenmiş kamera görüntüsü.
+    cv2.imshow("Red Mask", red_red_mask)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
